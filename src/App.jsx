95f0efb7c829a3114f8927842b9b1314dbbfabb7
Chtoś Niechtaŭski <mikhailalexv12@gmail.com>
2026-01-31 10:35:34 +0200

    deployment commit 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
-import { Routes, Route } from "react-router";
+import { Link, Routes, Route } from "react-router";
 import { MainPage } from "./pages/MainPage";
 import { RegisterPage } from "./pages/RegisterPage";
 import { useState } from "react";
 import cards from "./cards.json";
 import { CardPage } from "./pages/CardPage";
+import { createBrowserHistory } from "history";
 
 function App() {
-  const [isRegistered, setIsRegistered] = useState(false);
   return (
     <>
       <Routes>
+        <Route path="/"></Route>
         <Route index element={<MainPage />} />
         <Route path="cards/:id" element={<CardPage cards={cards} />} />
         <Route path="registration" element={<RegisterPage />} />
+        <Route
+          path="*"
+          element={
+            <div>
+              <h1>404: {window.location.pathname}</h1>
+              <Link to={"/"}>
+                <button>На главную</button>
+              </Link>
+            </div>
+          }
+        />
       </Routes>
     </>
   );
